e651963ed1c061f5bcd7a7d2e37e4191f260bad5
oroce <robert@oroszi.net>
2015-04-28 08:38:04 +0200

    initial commit

--- makepdf.sh
+++ makepdf.sh
@@ -1,1 +1,3 @@
-rubber --inplace  --warn all --force --pdf dolgozat.tex
+#!/usr/bin/env bash
+pdflatex --interaction=nonstopmode dolgozat.tex && bibtex dolgozat && pdflatex --interaction=nonstopmode dolgozat.tex && pdflatex --interaction=nonstopmode dolgozat.tex && makeindex dolgozat.nlo -s nomencl.ist -o dolgozat.nls && pdflatex --interaction=nonstopmode dolgozat.tex
+#rubber --inplace  --warn all --force --pdf dolgozat.tex
