4c3a6b24ee3504779cce6c7f43e39f8e51e4b79a
oroce <robert@oroszi.net>
2015-05-03 21:51:31 +0200

    grammar mistakes, summary and nomenclature

--- makepdf.sh
+++ makepdf.sh
@@ -1,3 +1,8 @@
 #!/usr/bin/env bash
-pdflatex --interaction=nonstopmode dolgozat.tex && bibtex dolgozat && pdflatex --interaction=nonstopmode dolgozat.tex && pdflatex --interaction=nonstopmode dolgozat.tex && makeindex dolgozat.nlo -s nomencl.ist -o dolgozat.nls && pdflatex --interaction=nonstopmode dolgozat.tex
+pdflatex --interaction=nonstopmode dolgozat.tex
+bibtex dolgozat
+pdflatex --interaction=nonstopmode dolgozat.tex 
+pdflatex --interaction=nonstopmode dolgozat.tex 
+makeindex dolgozat.nlo -s nomencl.ist -o dolgozat.nls 
+pdflatex --interaction=nonstopmode dolgozat.tex
 #rubber --inplace  --warn all --force --pdf dolgozat.tex
